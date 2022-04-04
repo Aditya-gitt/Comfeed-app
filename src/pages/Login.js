c413adc4a7cff7ea470e7b24f47dbfdd0a15b86f
Aditya-gitt <kulkarniaditya32@gmail.com>
2022-04-04 14:56:01 +0530

    - integrated singup and login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
+import {useNavigate } from 'react-router-dom';
+import axiosInstance from "../axios";
 import InputGroup from "../components/InputGroup";
 import Input from "../components/Input";
 import Button from "../components/Button";
@@ -20,6 +22,50 @@ const theme = {
     },
 };
 function Login() {
+
+    const navigate = new useNavigate();
+    const initialFormData = {
+        username : '',
+        password : '',
+    };
+
+    const [formData, updateFormData] = useState(initialFormData);
+    const [loading, loadingState] = useState(false);
+
+    const handleChange = (e) => {
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value.trim(),
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        loadingState(true);
+
+        axiosInstance
+            .post(`api/token/`, {
+                username : formData.username,
+                password : formData.password,
+            })
+            .then((response) => {
+                localStorage.setItem('access_token', response.data.access);
+                localStorage.setItem('refresh_token', response.data.refresh);
+                axiosInstance.defaults.headers['Authorization'] = 
+                'Bearer ' + response.data.access;
+                loadingState(false);
+                alert('You are logined as ' + formData.username);
+                navigate('/');
+            })
+            .catch((error) => {
+                loadingState(false);
+                const key = Object.keys(error.response.data)[0];
+                alert(JSON.stringify(error.response.data[key]).replace(/"/g, '')
+                                                              .replace('[', '')
+                                                              .replace(']', ''));
+            });
+    };
+
     return (
         // <ThemeProvider theme={theme}>
         //     <>
@@ -44,24 +90,30 @@ function Login() {
                     {/* <PageHeader to="/"> AWESOME LOGO</PageHeader> */}
                     <EntryCard>
                         <h2>Log in</h2>
-                        <form onSubmit={(e) => e.preventDefault()}>
+                        <form onSubmit={handleSubmit}>
                             <InputGroup>
                                 <label htmlFor="login-username"></label>
                                 <Input
                                     type="text"
+                                    required
                                     placeholder="username"
                                     id="login-username"
+                                    name='username'
+                                    onChange={handleChange}
                                 ></Input>
                             </InputGroup>
                             <InputGroup>
                                 <label htmlFor="login-password"></label>
                                 <Input
                                     type="text"
+                                    required
                                     placeholder="password"
                                     id="login-password"
+                                    name='password'
+                                    onChange={handleChange}
                                 ></Input>
                             </InputGroup>
-                            <Button type="submit">Log in </Button>
+                            {loading ? <Button >Loading... </Button> :<Button type="submit">Login</Button>}
                         </form>
                         <span>
                             Dont' have a account?
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
+import {useNavigate } from 'react-router-dom';
+import axiosInstance from "../axios";
 import InputGroup from "../components/InputGroup";
 import Input from "../components/Input";
 import Button from "../components/Button";
@@ -20,6 +22,49 @@ const theme = {
     },
 };
 function Signup() {
+
+    const navigate = new useNavigate();
+    const initialFormData = {
+        username : '',
+        password : '',
+    };
+
+    const [formData, updateFormData] = useState(initialFormData);
+    const [loading, loadingState] = useState(false);
+
+    const handleChange = (e) => {
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value.trim(),
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        loadingState(true);
+
+        axiosInstance
+            .post(`user/register/`, {
+                username : formData.username,
+                password : formData.password,
+            })
+            .then((response) => {
+                loadingState(false);
+                console.log(response);
+                console.log(response.data);
+                alert('Account Created successfully.');
+                navigate('/login');
+            })
+            .catch((error) => {
+                loadingState(false);
+                const key = Object.keys(error.response.data)[0];
+                alert(JSON.stringify(error.response.data[key]).replace(/"/g, '')
+                                                              .replace('[', '')
+                                                              .replace(']', ''));
+            });
+    };
+
+
     return (
         // <ThemeProvider theme={theme}>
         //     <>
@@ -43,25 +88,31 @@ function Signup() {
                 <EntryPage>
                     {/* <PageHeader to="/"> AWESOME LOGO</PageHeader> */}
                     <EntryCard>
-                        <h2>Log in</h2>
-                        <form onSubmit={(e) => e.preventDefault()}>
+                        <h2>Sign up</h2>
+                        <form onSubmit={handleSubmit}>
                             <InputGroup>
                                 <label htmlFor="Signup-username"></label>
                                 <Input
                                     type="text"
+                                    required
                                     placeholder="username"
                                     id="Signup-username"
+                                    name="username"
+                                    onChange={handleChange}
                                 ></Input>
                             </InputGroup>
                             <InputGroup>
                                 <label htmlFor="Signup-password"></label>
                                 <Input
                                     type="text"
+                                    required
                                     placeholder="password"
                                     id="Signup-password"
+                                    name="password"
+                                    onChange={handleChange}
                                 ></Input>
                             </InputGroup>
-                            <Button type="submit">Sign Up </Button>
+                            {loading ? <Button >Loading... </Button> :<Button type="submit">Sign Up </Button>}
                         </form>
                         <span>
                             Already have a account?
